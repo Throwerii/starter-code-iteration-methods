f89f7e0efb166b1d3b8c87afd658da4b91dff71f
Joe Thrower <jmthrower2@hotmail.com>
2020-07-01 15:09:41 -0400

    kata1-4 done

--- main.js
+++ main.js
@@ -3,7 +3,36 @@
 
 // The following line is here just to show you that the
 // "users" array has already been loaded and is ready to go.
+function printKata (kataNumber, object) {
+    
+    const detailsElement = document.createElement('details')
+    main.append(detailsElement)
+    //
+    const summaryElement = document.createElement('summary')
+    summaryElement.append("KATA " + kataNumber)
+    detailsElement.append(summaryElement)
+   
+    const stringifiedObject = JSON.stringify(object)
+    detailsElement.append(stringifiedObject)
+}
 console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+const active= users.filter(user => user.isActive === true)
+const map = users.map(function (user) {
+    return user.email 
+})
+const company = users.some(function (user) {
+    return user.company === "OVATION"
+})
+const age = users.find(function (user) {
+    return user.age>38
+})
+
+/
+    printKata(1, active)
+    printKata(2, map)
+    printKata(3, company)
+    printKata(4, age)
